0691d1315f9393c9f80ed692b8d87bff119eba31
Julian <julian.caceres@ipsos.com>
2019-03-20 01:16:38 +0000

    small update
    - Adding templates (base and home)
    - configurations and urls.

--- ipsos_ta/settings.py
+++ ipsos_ta/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'ipsos_ta.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,5 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
